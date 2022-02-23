a00cd98cb043b55da6161b7b281168699e38e800
Tyler Parris <tymania421@gmail.com>
2022-02-22 22:11:37 -0500

    Added RPS Functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function computerPlay(){
     console.log(cpuMove);
     switch (cpuMove) {
     case 1:
-        return result.textContent = 'Rock';
+        return 'Rock';
     case 2:
         return 'Scissors';
     case 3:
@@ -11,6 +11,61 @@ function computerPlay(){
     }
     
 }
+
+function playRound(playerSelection, computerSelection){
+    const playerInput = playerSelection.toLowerCase();
+    switch (playerInput){
+    case 'rock':
+        if(computerSelection == 'Rock'){
+            return 'Tie';
+        }
+        else if (computerSelection == 'Paper'){
+            return 'You Lose';
+        }
+        else {
+            return 'You Win';
+        }
+        break;
+    case 'paper':
+        if(computerSelection == 'Rock'){
+            return 'You Win!';
+        }
+        else if (computerSelection == 'Paper'){
+            return 'Tie';
+        }
+        else {
+            return 'You Lose';
+        }
+        break;
+    case 'scissors':
+        if(computerSelection == 'Rock'){
+            return 'You Lose';
+        }
+        else if (computerSelection == 'Paper'){
+            return 'You Win!';
+        }
+        else {
+            return 'Tie';
+        }
+        break;
+    default:
+        return 'Invalid Input. Try using Rock, Paper or Scissors';
+        break;
+    }
+}
+
+function game(){
+    let userInput = window.prompt("Let's Play RPS.")
+    
+        
+        let cpuInput = computerPlay();
+        console.log(cpuInput);
+        let result2 = playRound(userInput, cpuInput);
+        result.textContent = 'Computer chose ' + cpuInput + ' And the result is ' + result2;
+    
+
+}
+
 const result = document.querySelector('.result');
 const randomize = document.querySelector('.randomize');
-randomize.addEventListener('click', computerPlay);
+randomize.addEventListener('click', game);
